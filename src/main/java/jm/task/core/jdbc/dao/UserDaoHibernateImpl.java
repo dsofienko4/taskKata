2aaee011b1872c3e5ec4cac51f34f76b9499cf9d
dsofienko4 <dsofienko1@gmail.com>
2023-03-01 15:57:28 +0300

    resOf_1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -29,7 +29,7 @@ public class UserDaoHibernateImpl implements UserDao {
     public void removeUserById(long id) {
 
     }
-
+//
     @Override
     public List<User> getAllUsers() {
         return null;
